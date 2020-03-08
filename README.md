fc7e9283fa988e2bdd0bbf33e326069dcdec0195
Liangqin Sun <liansun@microsoft.com>
2020-03-08 12:54:07 -0700

    WIP 2020-03-08T12:54:07

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
+# Introduction
+This extension enables you quickly switch between interested files. Press `Ctrl+Alt+S` to switch to another file from quick pick.
+
 ![Quick Switch](images/quick-switch.gif)
 
+# Setting
 The following setting enables quick switch among .cpp, .h and _ut.cpp files.
 
 ```json
@@ -16,13 +20,3 @@ The following setting enables quick switch among .cpp, .h and _ut.cpp files.
 ```
 1. pattern: a regular expression to match with the current file path. The first group that matches will be used for quick switch.
 1. list: an ordered list of files for quick switch. Use $n to reference the capturing groups defined in pattern.
-
-When you are in one of the .cpp, .h or _ut.cpp file, you can
-* Press `Ctrl+Alt+N` to switch to the next file in the list
-* Press `Ctrl+Alt+P` to switch to the previous file in the list
-* Press `Ctrl+Alt+S` to switch to a file from quick pick
-* Press `Ctrl+Alt+1` to switch to the .cpp file (the first file in the list)
-* Press `Ctrl+Alt+2` to switch to the .h file (the second file in the list)
-* Press `Ctrl+Alt+3` to switch to the _ut.cpp file (the third file in the list)
-* Press `Ctrl+Alt+L` to compare with a file from quick pick (current file as left file)
-* Press `Ctrl+Alt+R` to compare with a file from quick pick (current file as right file)
--- src/extension.ts
+++ src/extension.ts
@@ -1,44 +1,29 @@
 import * as fs from 'fs';
-import *  as path from 'path';
+import * as path from 'path';
 import * as vscode from 'vscode';
 
 const map = new Map<string, string[]>();
 
 class FileGroup {
-    pattern: string;
-    list: string[];
-
-    constructor(pattern: string, list: string[]) {
-        this.pattern = pattern;
-        this.list = list;
-    }
+    constructor(public readonly pattern: string, public readonly list: string[]) { }
 
     match(file: string) {
         return file.match(this.pattern);
     }
 
-    getList(file: string) {
-        const result = [];
-        for (const item of this.list) {
-            const filePath = file.replace(new RegExp(this.pattern), item);
-            result.push(path.resolve(filePath));
-        }
-
-        return result;
+    expand(file: string) {
+        return this.list.map(x => {
+            const filePath = file.replace(new RegExp(this.pattern), x);
+            return path.resolve(filePath);
+        });
     }
 }
 
 class Configuration {
     static Load() {
-        const fileGroups = [];
-        const fileGroupsConfig = vscode.workspace.getConfiguration().get<FileGroup[]>('quickSwitch.fileGroups');
-        if (fileGroupsConfig) {
-            for (const fileGroupConfig of fileGroupsConfig) {
-                fileGroups.push(new FileGroup(fileGroupConfig.pattern, fileGroupConfig.list));
-            }
-        }
-
-        return fileGroups;
+        const fileGroupsConfig = vscode.workspace.getConfiguration().get<FileGroup[]>(
+            'quickSwitch.fileGroups', []);
+        return fileGroupsConfig.map(x => new FileGroup(x.pattern, x.list));
     }
 }
 
@@ -47,201 +32,44 @@ class FileQuickPickItem implements vscode.QuickPickItem {
     }
 }
 
-function getCurrentFilePath() {
-    const activeTextEditor = vscode.window.activeTextEditor;
-    if (!activeTextEditor) {
-        return;
-    }
+async function selectFileFromPick(files: string[]) {
+    const fileQuickPickItems = files.map(x => {
+        const relativePath = vscode.workspace.asRelativePath(x);
+        return new FileQuickPickItem('$(file)  ' + relativePath, x);
+    });
 
-    return activeTextEditor.document.fileName;
-}
-
-async function actionOnSelectedFile(
-    filterCallback: (currentIndex: number, files: string[]) => Promise<string | undefined>,
-    actionCallback: (filePath: string) => void) {
-
-    const currentFilePath = getCurrentFilePath();
-    if (!currentFilePath) {
-        return;
-    }
-
-    const fileGroups = Configuration.Load();
-    for (const fileGroup of fileGroups) {
-        let files;
-        if (fileGroup.match(currentFilePath)) {
-            files = fileGroup.getList(currentFilePath);
-        }
-        else {
-            files = map.get(currentFilePath) || [];
-        }
-        if (files.length === 0) {
-            continue;
-        }
-
-        const currentIndex = files.indexOf(currentFilePath);
-        const file = await filterCallback(currentIndex, files);
-        if (!file || !fs.existsSync(file)) {
-            return;
-        }
-
-        if (!fileGroup.match(file)) {
-            map.set(file, files);
-        }
-
-        actionCallback(file);
-        return;
-    }
-}
-
-async function selectFileFromPick(currentIndex: number, files: string[]) {
-    const fileQuickPickItems = [];
-    for (const file of files) {
-        if (!fs.existsSync(file)) {
-            continue;
-        }
-
-        const relativePath = vscode.workspace.asRelativePath(file);
-        fileQuickPickItems.push(new FileQuickPickItem('$(file)  ' + relativePath, file));
-    }
-
-    if (fileQuickPickItems.length === 0) {
-        return;
-    }
-
-    const selectedFile = await vscode.window.showQuickPick(fileQuickPickItems);
-    if (!selectedFile) {
-        return;
-    }
-
-    return selectedFile.path;
-}
-
-function showFile(filePath: string) {
-    vscode.workspace.openTextDocument(filePath).then(vscode.window.showTextDocument);
-}
-
-function compareFiles(leftFilePath: string, rightFilePath: string) {
-    const leftFileUri = vscode.Uri.file(leftFilePath);
-    const rightFileUri = vscode.Uri.file(rightFilePath);
-    const title = `${path.basename(leftFilePath)} ↔ ${path.basename(rightFilePath)}`;
-
-    return vscode.commands.executeCommand('vscode.diff', leftFileUri, rightFileUri, title);
+    const selectedFile = await vscode.window.showQuickPick(fileQuickPickItems,
+        { placeHolder: "Switch file" });
+    return selectedFile?.path;
 }
 
 export function activate(context: vscode.ExtensionContext) {
-    context.subscriptions.push(
-        vscode.commands.registerCommand(
-            'extension.compareAsLeftFile',
-            () => actionOnSelectedFile(selectFileFromPick, (filePath) => {
-                const currentFilePath = getCurrentFilePath();
-                if (!currentFilePath) {
-                    return;
-                }
+    context.subscriptions.push(vscode.commands.registerCommand('extension.switchFile', async () => {
+        const activeTextEditor = vscode.window.activeTextEditor;
+        if (!activeTextEditor) {
+            return;
+        }
+        const currentFilePath = activeTextEditor.document.fileName;
 
-                compareFiles(currentFilePath, filePath);
-            }))
-    );
+        const fileGroups = Configuration.Load();
+        const matchedFileGroup = fileGroups.find(x => x.match(currentFilePath));
+        let files = matchedFileGroup?.expand(currentFilePath) || map.get(currentFilePath) || [];
 
-    context.subscriptions.push(
-        vscode.commands.registerCommand(
-            'extension.compareAsRightFile',
-            () => actionOnSelectedFile(selectFileFromPick, (filePath) => {
-                const currentFilePath = getCurrentFilePath();
-                if (!currentFilePath) {
-                    return;
-                }
+        const candidates = files.filter(x => x != currentFilePath && fs.existsSync(x));
+        if (candidates.length == 0) {
+            return;
+        }
 
-                compareFiles(filePath, currentFilePath);
-            }))
-    );
+        const file = await selectFileFromPick(candidates);
+        if (!file) {
+            return;
+        }
 
-    context.subscriptions.push(
-        vscode.commands.registerCommand(
-            'extension.switchFile',
-            () => actionOnSelectedFile(selectFileFromPick, showFile)
-        ));
+        if (!matchedFileGroup?.match(file)) {
+            map.set(file, files);
+        }
 
-    context.subscriptions.push(
-        vscode.commands.registerCommand(
-            'extension.switchNextFile',
-            () => {
-                actionOnSelectedFile(
-                    async (currentIndex, files) => {
-                        for (let i = 1; i < files.length; i++) {
-                            const file = files[(currentIndex + i) % files.length];
-                            if (fs.existsSync(file)) {
-                                return file;
-                            }
-                        }
-                    }, showFile);
-            }));
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand(
-            'extension.switchPreviousFile',
-            () => {
-                actionOnSelectedFile(
-                    async (currentIndex, files) => {
-                        for (let i = 1; i < files.length; i++) {
-                            const file = files[(currentIndex - i + files.length) % files.length];
-                            if (fs.existsSync(file)) {
-                                return file;
-                            }
-                        }
-                    }, showFile);
-            }));
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand(
-            'extension.switchAtIndex1',
-            () => actionOnSelectedFile(async (currentIndex, files) => files[0], showFile)
-        ));
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand(
-            'extension.switchAtIndex2',
-            () => actionOnSelectedFile(async (currentIndex, files) => files[1], showFile)
-        ));
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand(
-            'extension.switchAtIndex3',
-            () => actionOnSelectedFile(async (currentIndex, files) => files[2], showFile)
-        ));
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand(
-            'extension.switchAtIndex4',
-            () => actionOnSelectedFile(async (currentIndex, files) => files[3], showFile)
-        ));
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand(
-            'extension.switchAtIndex5',
-            () => actionOnSelectedFile(async (currentIndex, files) => files[4], showFile)
-        ));
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand(
-            'extension.switchAtIndex6',
-            () => actionOnSelectedFile(async (currentIndex, files) => files[5], showFile)
-        ));
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand(
-            'extension.switchAtIndex7', () =>
-            actionOnSelectedFile(async (currentIndex, files) => files[6], showFile)
-        ));
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand(
-            'extension.switchAtIndex8', () =>
-            actionOnSelectedFile(async (currentIndex, files) => files[7], showFile)
-        ));
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand(
-            'extension.switchAtIndex9', () =>
-            actionOnSelectedFile(async (currentIndex, files) => files[8], showFile)
-        ));
+        const document = await vscode.workspace.openTextDocument(file);
+        vscode.window.showTextDocument(document);
+    }));
 }
